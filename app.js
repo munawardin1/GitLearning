55ace08d3ef3ce82a00ddf1904bff228daf81ea9
Fazloo <munawardin704@gmail.com>
2026-09-25 20:46:03 +0500

    background changin

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ const btn = document.getElementById("myBtn");
 
 btn.addEventListener("click", () => {
   btn.textContent = "You clicked me!";
+  document.body.style.backgroundColor = "lightgreen";
 });
